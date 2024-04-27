027b82facd82b684a6a32bb78fdce58ff8060c68
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-27 19:13:21 +0530

    modified Navbar.jsx added user.png

--- my-app/app/components/Navbar.jsx
+++ my-app/app/components/Navbar.jsx
@@ -1,11 +1,32 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "./user.png";
+
 export default function Navbar() {
   return (
     <div>
-      <nav>
-        <h1>Shanaka Prince</h1>
-        <Link href="/">Dasboard</Link>
-        <Link href="/tickets">Tickets</Link>
+      <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+        <div className="flex items-center space-x-4">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={70}
+            height={70}
+            quality={100}
+            placeholder="blur"
+          />
+
+          <h1 className="text-lg font-bold">Shanaka Prince</h1>
+        </div>
+
+        <div className="flex space-x-4">
+          <Link href="/" passHref>
+            Dashboard
+          </Link>
+          <Link href="/tickets" passHref>
+            Tickets
+          </Link>
+        </div>
       </nav>
     </div>
   );
