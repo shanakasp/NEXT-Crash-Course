f0ab45348e902743f965168b1e719d5f6d877a30
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-27 20:54:57 +0530

    modified TicketList.jsx added page.jsx

--- my-app/app/tickets/TicketList.jsx
+++ my-app/app/tickets/TicketList.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
@@ -17,11 +19,13 @@ export default async function TicketList() {
       <h3>
         {tickets.map((ticket) => (
           <div key={ticket.id} className="card my-5">
-            <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0, 200)}...</p>
-            <div className={`pill ${ticket.priority}`}>
-              {ticket.priority} Priority
-            </div>
+            <Link href={`/tickets/${ticket.id}`}>
+              <h3>{ticket.title}</h3>
+              <p>{ticket.body.slice(0, 200)}...</p>
+              <div className={`pill ${ticket.priority}`}>
+                {ticket.priority} Priority
+              </div>
+            </Link>
           </div>
         ))}
         {tickets.length === 0 && (
@@ -31,3 +35,5 @@ export default async function TicketList() {
     </div>
   );
 }
+
+// Tickets/id
